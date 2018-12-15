27a86a51ea259bb9879c2ae3846e48d6a962d297
David Frech <david@nimblemachines.com>
2018-12-14 23:27:56 -0800

    Now prints out register fields too!
    I think I might be done here. Except maybe to try to find newer versions
    of the SVD files. These are all from the Kinetis SDK V1.30 (KSDK_1.3.0/)
    which seems to date from 2015.

--- print-regs.lua
+++ print-regs.lua
@@ -53,13 +53,31 @@ function print_regs(chip)
 
     as_equates = visit(
         function(k, v, path, ctx)
+            local print_reg = function()
+                local name = (ctx.periph.prepend_to_name or "") .. ctx.reg.name
+                local addr = tonumber(ctx.periph.base_address) + tonumber(ctx.reg.address_offset)
+                local descr = (ctx.reg.description and "| "..ctx.reg.description) or ""
+                if ctx.reg.dim then
+                    -- Print multiple registers
+                    local offset = 0
+                    for i in ctx.reg.dim_index:gmatch "%w+" do
+                        io.write(fmt("%s equ    %-28s %s\n", muhex(addr + offset), fmt(name, i), unparen(descr)))
+                        offset = offset + ctx.reg.dim_increment
+                    end
+                else
+                    io.write(fmt("%s equ    %-28s %s\n", muhex(addr), name, unparen(descr)))
+                end
+                ctx.reg.printed = true
+            end
+
             path = path.."/"..k
             if path == "/peripherals" then
                 -- print chip info
                 io.write "( Automagically generated! DO NOT EDIT!)\n\n"
-                io.write(fmt("( %s %s %s equates, SVD version %s)\n",
+                io.write(fmt("( %s %s %s equates. Generated from %s.svd, version %s)\n",
                     ctx.chip.vendor_id,
                     ctx.chip.series,
+                    ctx.chip.name,
                     ctx.chip.name,
                     ctx.chip.version))
             elseif path == "/peripherals/peripheral" then
@@ -71,24 +89,33 @@ function print_regs(chip)
             elseif path == "/peripherals/peripheral/registers/register" then
                 -- reset context
                 ctx.reg = {}
+            elseif path == "/peripherals/peripheral/registers/register/fields" then
+                -- Generally fields follow the register def, so print the reg now
+                print_reg()
+            elseif path == "/peripherals/peripheral/registers/register/fields/field" then
+                -- reset context
+                ctx.field = {}
             end
 
             -- Recurse into subtable
             as_equates(v, path, ctx)
 
             if path == "/peripherals/peripheral/registers/register" then
-                local name = (ctx.periph.prepend_to_name or "") .. ctx.reg.name
-                local addr = tonumber(ctx.periph.base_address) + tonumber(ctx.reg.address_offset)
-                local descr = (ctx.reg.description and "| "..ctx.reg.description) or ""
-                if ctx.reg.dim then
-                    -- Print multiple registers
-                    local offset = 0
-                    for i in ctx.reg.dim_index:gmatch "%w+" do
-                        io.write(fmt("%s equ %-20s %s\n", muhex(addr + offset), fmt(name, i), unparen(descr)))
-                        offset = offset + ctx.reg.dim_increment
-                    end
-                else
-                    io.write(fmt("%s equ %-20s %s\n", muhex(addr), name, unparen(descr)))
+                -- If register has no fields, we won't have printed it above. Do it now.
+                if not ctx.reg.printed then
+                    print_reg()
+                end
+            elseif path == "/peripherals/peripheral/registers/register/fields/field" then
+                -- Registers defined with "dim" also have fields... but
+                -- printing the fields for each version of the register
+                -- would be stupid. Let's instead just remove the "%s" from
+                -- the name.
+                if ctx.field.bit_width < ctx.reg.size then
+                    local name = (ctx.periph.prepend_to_name or "") ..
+                                  ctx.reg.name:gsub("%%s", "")  .. "_" .. ctx.field.name
+                    local descr = (ctx.field.description and "| "..ctx.field.description) or ""
+                    io.write(fmt("  #%02d #%02d field  %-28s %s\n", ctx.field.bit_offset, ctx.field.bit_width,
+                                                                   name, descr))
                 end
             end
         end,
@@ -97,11 +124,11 @@ function print_regs(chip)
             if path == "" then
                 ctx.chip[k] = v
             elseif path == "/peripherals/peripheral" then
-                --print(fmt("periph.%s = %s", k, v))
                 ctx.periph[k] = v
             elseif path == "/peripherals/peripheral/registers/register" then
-                --print(fmt("reg.%s = %s", k, v))
                 ctx.reg[k] = v
+            elseif path == "/peripherals/peripheral/registers/register/fields/field" then
+                ctx.field[k] = v
             end
         end)
 
